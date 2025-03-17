8f31f9a7ce5405a6bc76a9c018ed653d6efc2bfe
theo.fratia <theofratia22@gmail.com>
2025-03-17 11:17:06 +0100

    rename screen folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mobile/screen/home_screen.dart';
+import 'package:mobile/screens/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
