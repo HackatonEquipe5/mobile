461d9071b2c221f40383fa3597cb5a0202389a27
theo.fratia <theofratia22@gmail.com>
2025-03-17 11:05:33 +0100

    clean

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screen/Home.dart';
+import 'screen/home.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: const MyHomePage(title: 'Flutter Hello World'),
+      home: MyHomePage(title: 'Flutter Hello World'),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:mobile/main.dart';
-import 'package:mobile/screen/Home.dart';
+import 'package:mobile/screen/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
