29d4b1e84d2ecab13a723b38295e87fc541e6e39
theo.fratia <theofratia22@gmail.com>
2025-03-18 12:21:24 +0100

    Add id

--- lib/models/coffee_machine.dart
+++ lib/models/coffee_machine.dart
@@ -1,6 +1,7 @@
 class CoffeeMachine {
   final String name;
   final String? imageUrl;
+  String id;
   bool isFavorite;
   bool isWorking;
 
@@ -8,6 +9,7 @@ class CoffeeMachine {
     required this.name,
     this.imageUrl,
     this.isFavorite = false,
+    this .id = "00-01",
     required this.isWorking,
   });
 }
